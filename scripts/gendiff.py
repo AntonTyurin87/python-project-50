7472950af1e52bd7f76e3b5cee4d668ad06615b9
Anton Tyurin <AntonTyurin87@gmail.com>
2023-03-02 11:44:49 +0300

    Try to do working script

--- scripts/gendiff.py
+++ scripts/gendiff.py
@@ -1,7 +1,14 @@
 #Здесь будет скрипт с вызовом argparse 
 
+import argparse
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+    print(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
