834569eaada8352efa66402c59c044bff254b52f
Anton Tyurin <AntonTyurin87@gmail.com>
2023-03-06 15:12:38 +0300

    Created files to step_3 in project.

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,14 @@
+# Здесь будет функция поиска отличий файлов json
+
+import json
+
+def generate_diff():
+    pass
+
+a = json.load(open('fixtures/file1.json'))
+
+print(a)
+
+'''
+json.load(open('path/to/file.json'))
+'''
