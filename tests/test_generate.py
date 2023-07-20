13aa111cebb71237213c18a9fb2936873afc7b26
AntonTyurin87 <AntonTyurin87gmail.com>
2023-07-20 13:51:22 +0300

    Step number 4.3.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,11 +1,12 @@
+from gendiff.generate_d import generate_diff
 
 
 def test_generate_diff():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == {
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == '''{
     - follow: False
       host: hexlet.io
     - proxy: 123.234.53.22
     - timeout: 50
     + timeout: 20
     + verbose: True
-}
+}'''
