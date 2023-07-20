b1849dd866cbe082fac5f25d67eee0e0cbd14313
AntonTyurin87 <AntonTyurin87gmail.com>
2023-07-20 10:01:23 +0300

    Change return to string.

--- gendiff/generate_d.py
+++ gendiff/generate_d.py
@@ -8,6 +8,7 @@ file2 = 'fixtures/file2.json'
 def generate_diff(file1, file2):
     result_dict = {}
     result_list = []
+    result_str = ''
 
     file_path1 = json.load(open(file1))
     file_path2 = json.load(open(file2))
@@ -30,5 +31,11 @@ def generate_diff(file1, file2):
     for k in sorted(result_list, key=lambda point: (point[0])):
         result_dict.update({k[1] + ' ' + k[0]: k[2]})
 
+    for d in result_dict:
+        result_str += '    ' + str(d) + ':' + ' ' + str(result_dict.get(d)) + '\n'
+
+    result_str = '{' + '\n' + result_str + '}'
+
+    return result_str
     # return result_dict
-    return json.dumps(result_dict, indent=4, sort_keys=False)
+    # return json.dumps(result_dict, indent=4, sort_keys=False)
