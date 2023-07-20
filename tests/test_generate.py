b7322d51553ccdb8c56f17f51c05a51e88bed6ea
AntonTyurin87 <AntonTyurin87@gmail.com>
2023-07-20 20:01:21 +0300

    Fix problem with pytest.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 def test_generate_diff():
-    assert generate_diff.generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == '''{
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == '''{
     - follow: False
       host: hexlet.io
     - proxy: 123.234.53.22
