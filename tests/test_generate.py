b96c2e64c51fee6510a37e12148f9b5c8a5b25e3
AntonTyurin87 <AntonTyurin87gmail.com>
2023-07-21 13:32:44 +0300

    Add tests_16.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -12,3 +12,13 @@ def test_generate_diff():
     + timeout: 20
     + verbose: True
 }'''
+
+
+def test_type_test_generate_diff():
+    text = 'text'
+    assert type(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')) == type(text)
+
+
+def len_text_test_generate_diff():
+    assert len(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')) == 128
+    
